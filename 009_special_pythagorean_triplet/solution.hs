4d9ca48ac4aaded5fbf2015c7d8873155e9ad631
ahartel <ahartel@gmx.de>
2017-10-19 10:27:56 +0200

    Fixed solution to problem 9
    - ambiguity in definition of sum (renamed)
    - needed show to putStrLn a list

--- 009_special_pythagorean_triplet/solution.hs
+++ 009_special_pythagorean_triplet/solution.hs
@@ -1,6 +1,6 @@
 ptr n m = (m*m-n*n, 2*n*m, n*n+m*m)
-sum (x,y,z) = x+y+z
+sumtriple (x,y,z) = x+y+z
 prod (x,y,z) = x*y*z
 main = do
-  putStrLn $ take 1 $ dropWhile (\(x,y) -> x /= 1000) ptrSumProd
-    where ptrSumProd = [(sum (ptr m n), prod (ptr m n)) | m<-[1..20], n<-[m+1..20]]
+  putStrLn $ show $ take 1 $ dropWhile (\(x,y) -> x /= 1000) ptrSumProd
+    where ptrSumProd = [(sumtriple (ptr m n), prod (ptr m n)) | m<-[1..20], n<-[m+1..20]]
